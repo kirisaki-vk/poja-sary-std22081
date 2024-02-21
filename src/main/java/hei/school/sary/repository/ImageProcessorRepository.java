875be15f6d043990139749abfb401ab7d6759134
kirisaki_vk <kirisaki.vk@gmail.com>
2024-02-21 21:01:32 +0300

    fix: repository type mismatch

--- src/main/java/hei/school/sary/repository/ImageProcessorRepository.java
+++ src/main/java/hei/school/sary/repository/ImageProcessorRepository.java
@@ -1,9 +1,8 @@
 package hei.school.sary.repository;
 
 import hei.school.sary.repository.model.ImageProcess;
-import java.util.UUID;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface ImageProcessorRepository extends JpaRepository<ImageProcess, UUID> {}
+public interface ImageProcessorRepository extends JpaRepository<ImageProcess, String> {}
